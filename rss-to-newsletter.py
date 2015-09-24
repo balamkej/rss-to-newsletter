32bf7589344ea01bea447874054553587c0976ae
Robert Henderson <rhenderson@mail.arizona.edu>
2015-09-24 10:11:18 -0700

    Started newsList function.

--- rss-to-newsletter.py
+++ rss-to-newsletter.py
@@ -5,6 +5,10 @@ from bs4 import BeautifulSoup
 
 d = feedparser.parse('http://linguistics.arizona.edu/rss.xml')
 
+# Takes a start date and end date spead and returns a list 
+# of d.entries[n] items with a publication date in that spread.
+def newsList(start,end):
+
 # Takes an d.entries[n] item and returns a formatted string 
 # with title, date, body, and link of item.
 def newsItem(entry):
