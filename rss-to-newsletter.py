d85892734061604a89cc5bc5e2d1d3150466128e
Robert Henderson <rhenderson@mail.arizona.edu>
2015-10-05 20:28:04 -0700

    Changed to html formatting

--- rss-to-newsletter.py
+++ rss-to-newsletter.py
@@ -5,7 +5,7 @@ import feedparser
 from bs4 import BeautifulSoup
 
 d = feedparser.parse('http://linguistics.arizona.edu/rss.xml')
-f = open('newsfile.txt', 'w')
+f = open('newsfile.html', 'w')
 
 # Returns a list of news entries from feed that are less than 15 days old.
 def newsList():
@@ -20,13 +20,11 @@ def newsList():
 # with title, date, body, and link of item.
 def newsItem(entry):
 	soup = BeautifulSoup(entry.description, 'html.parser')
-	return entry.title + "\n\n" + soup.p.get_text() + "\n\n" + entry.link + "\n\n\n"
+	return "<h3><a href=\"" + entry.link + "\" style=\"color:rgb(171,5,32); text-decoration:none\">" + entry.title + "</a></h3>" + "\n\n" + "<p>" +soup.p.get_text() + "</p>" + "\n\n\n"
 
-f.write("WHAA as of " + time.strftime('%x') + "\n\n" + "As always, you can catch the latest UA Linguistics news, including photos, at http://linguistics.arizona.edu/news. Below are this edition's highlights:" + "\n\n\n")
+f.write("<a href=\"http://linguistics.arizona.edu\"><img src=\"http://www.powerwarg.com/UASBS_LINGUISTICS_PRIMARY.png\" width=\"400\" height=\"75\"></a>" + "\n" + "<h2><font color=\"rgb(12,35,75)\">WHAAL " + time.strftime('%x') + " Edition</font></h2>" + "\n\n" + "<p>As always, you can catch the latest UA Linguistics news, including photos, at our <a href=\"http://linguistics.arizona.edu/news\" style=\"color:rgb(171,5,32); text-decoration:none\">news feed</a>." + " Remember that whenever anything is going on in your linguistics life, be sure to <a href=\"http://linguistics.arizona.edu/eform/submit/news-submitted\" style=\"color:rgb(171,5,32); text-decoration: none\">submit</a> it to WHAAL." + "\n\n\n")
 
 for entry in newsList():
 	f.write(newsItem(entry))
-
-f.write("Remember that whenever anything is going on in your linguistics life, be sure to send it to rhenderson@email.arizona.edu or submit it at our website: http://linguistics.arizona.edu/eform/submit/news-submitted.")
 
 f.close()
